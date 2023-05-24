33f9ce7efe62a6052e75431f85fb782ba0082908
Pabs <paul.mca.ca@gmail.com>
2023-05-24 10:12:40 +0000

    Add fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -1,7 +1,6 @@
 const request = require('request');
 
 const fetchMyIP = function(callback) { 
-
   request("https://api.ipify.org?format=json", (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -16,7 +15,30 @@ const fetchMyIP = function(callback) {
     
     const ip = JSON.parse(body).ip;
     callback(null, ip);
+    return;
   })
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, { latitude, longitude });
+  })
+  
+}
+
+module.exports = { fetchCoordsByIP, fetchMyIP };
