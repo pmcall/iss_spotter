4c3a978bd72c2c395afb40e0a1b326d902097e07
Pabs <paul.mca.ca@gmail.com>
2023-05-24 10:08:44 +0000

    Refactor: cleaned up file structure and added test for invalid IP being returned

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +8,17 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+
+  // fetchCoordsByIP(ip, (error, location) => {
+  //   if (error) {
+  //     console.log("It didn't work!" , error);
+  //     return;
+  //   }
+  
+  //   console.log('It worked! Returned location:' , location);
+  // });
 });
+
+
+
+
